ce1b5a7fe66a817ffc1dcf6def0c3424a9a779c0
Kaio <luizkaio9815@gmail.com>
2021-09-28 19:20:50 -0300

    Corrigido erro de posição

--- desafioPedraPapelETesoura.js
+++ desafioPedraPapelETesoura.js
@@ -2,48 +2,48 @@
 /*2*/var papelVsPedra = 'Papel cobre Veneno!';
      var afterpapelVsPedra;
      afterpapelVsPedra = papelVsPedra.replace("Veneno", "Pedra");
-     afterpapelVsPedra = papelVsPedra.replace("cobre", "embrulha");
+     afterpapelVsPedra = afterpapelVsPedra.replace("cobre", "embrulha");
 
 /*3*/var pedraVsTesoura = 'Pedra esmaga Lagarto!';
      var afterpedraVsTesoura;
      afterpedraVsTesoura = pedraVsTesoura.replace("esmaga", "quebra");
-     afterpedraVsTesoura = pedraVsTesoura.replace("Lagarto", "Tesoura");
+     afterpedraVsTesoura = afterpedraVsTesoura.replace("Lagarto", "Tesoura");
 
 /*4*/var tesouraVsPapel2 = 'Lagarto corta Spock!';
      var aftertesouraVsPapel2;
      aftertesouraVsPapel2 = tesouraVsPapel2.replace("Lagarto", "Tesoura");
-     aftertesouraVsPapel2 = tesouraVsPapel2.replace("Spock", "Papel");
+     aftertesouraVsPapel2 = aftertesouraVsPapel2.replace("Spock", "Papel");
 
 /*5*/var papelVsTesoura = 'Spock vaporiza Tesoura!';
      var afterpapelVsTesoura;
      afterpapelVsTesoura = papelVsTesoura.replace("Spock", "Papel");
-     afterpapelVsTesoura = papelVsTesoura.replace("vaporiza", "embrulha");
+     afterpapelVsTesoura = afterpapelVsTesoura.replace("vaporiza", "embrulha");
 
 /*6*/var tesouraVsPapel3 = 'Tesoura decapita Spock!';
      var aftertesouraVsPapel3;
      aftertesouraVsPapel3 = tesouraVsPapel3.replace("decapita", "corta");
-     aftertesouraVsPapel3 = tesouraVsPapel3.replace("Spock", "Papel");
+     aftertesouraVsPapel3 = aftertesouraVsPapel3.replace("Spock", "Papel");
 
 /*7*/var tesouraVsPapel4 = 'Lagarto come Papel!';
      var aftertesouraVsPapel4;
      aftertesouraVsPapel4 = tesouraVsPapel4.replace("Lagarto", "Tesoura");
-     aftertesouraVsPapel4 = tesouraVsPapel4.replace("come", "corta");
+     aftertesouraVsPapel4 = aftertesouraVsPapel4.replace("come", "corta");
 
 /*8*/var pedraVsTesoura2 = 'Veneno refuta Lagarto!';
      var afterpedraVsTesoura2;
      afterpedraVsTesoura2 = pedraVsTesoura2.replace("Veneno", "Pedra");
-     afterpedraVsTesoura2 = pedraVsTesoura2.replace("refuta", "quebra");
-     afterpedraVsTesoura2 = pedraVsTesoura2.replace("Lagarto", "Tesoura");
+     afterpedraVsTesoura2 = afterpedraVsTesoura2.replace("refuta", "quebra");
+     afterpedraVsTesoura2 = afterpedraVsTesoura2.replace("Lagarto", "Tesoura");
 
 /*9*/var papelVsPedra2 = 'Spock vaporiza Pedra!';
      var afterpapelVsPedra2;
      afterpapelVsPedra2 = papelVsPedra2.replace("Spock", "Papel");
-     afterpapelVsPedra2 = papelVsPedra2.replace("vaporiza", "embrulha");
+     afterpapelVsPedra2 = afterpapelVsPedra2.replace("vaporiza", "embrulha");
 
 /*10*/var pedraVsTesoura3 = 'Veneno esmaga Tesoura!';
       var afterpedraVsTesoura3;
       afterpedraVsTesoura3 = pedraVsTesoura3.replace("Veneno", "Pedra");
-      afterpedraVsTesoura3 = pedraVsTesoura3.replace("esmaga", "quebra");
+      afterpedraVsTesoura3 = afterpedraVsTesoura3.replace("esmaga", "quebra");
 
 //var game = [tesouraVsPaPel, papelVsPedra, pedraVsTesoura, tesouraVsPapel2, papelVsTesoura, 
 //   tesouraVsPapel3, tesouraVsPapel4, pedraVsTesoura2, papelVsPedra2, pedraVsTesoura3];
